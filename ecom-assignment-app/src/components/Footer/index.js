db3d33a008bfb1e99d7ceab729d2b9851a247ef4
Emel-H <emel-emel99@hotmail.com>
2024-04-05 22:19:13 +0200

    fixes cosmetics

--- ecom-assignment-app/src/components/Footer/index.js
+++ ecom-assignment-app/src/components/Footer/index.js
@@ -2,8 +2,14 @@ import React from "react";
 
 function Footer() {
     return (
-      <footer>
-        this is my footer
+      <footer className="bg-dark text-info">
+        <p className="mt-2">Follow us:</p>
+        <p>
+            <a className="btn-sm btn-light mr-2" href="https://www.facebook.com" >FB</a>
+            <a className="btn-sm btn-light mr-2" href="https://www.twitter.com" >TW</a>
+            <a className="btn-sm btn-light mr-2" href="https://www.instagram.com" >IG</a>
+        </p>
+        <p>&copy;2024 eCOM Ltd</p>
       </footer>
     );
   }
